cbe1e1abbf4e61d5ef382173d244b04747fe8763
Lev K <lev_k@malam-payroll.com>
2024-08-27 21:56:04 +0300

    added instructions

--- scripts/index.js
+++ scripts/index.js
@@ -47,20 +47,20 @@ $(function() {
         }
 
         // Link Highlighting
-        if (pos2 > $('#home').offset().top) {
+        if (pos2 > $('#home')?.offset().top) {
             highlightLink('home');
         }
-        if (pos2 > $('#about').offset().top) {
+        if (pos2 > $('#about')?.offset().top) {
             highlightLink('about');
         }
-        if (pos2 > $('#portfolio').offset().top) {
+        if (pos2 > $('#portfolio')?.offset().top) {
             highlightLink('portfolio');
         }
-        if (pos2 > $('#blog').offset().top) {
+        if (pos2 > $('#blog')?.offset().top) {
             highlightLink('blog');
         }
         if (
-            pos2 > $('#contact').offset().top ||
+            pos2 > $('#contact')?.offset().top ||
             pos + $(window).height() === $(document).height()
         ) {
             highlightLink('contact');
